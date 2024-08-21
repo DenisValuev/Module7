bdb8c01674fdd16535d3f3beb3da3b4091f91d3e
DeniValuev <valuev.denis91@mail.ru>
2024-08-21 20:48:36 +0300

    Model7-master: add new Unit4

---
+++ Module7.Unit4/Models/SystemUnit.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module7.Unit4.Models
+{
+    internal class SystemUnit
+    {
+        private MotherBoard motherBoard;
+        public SystemUnit()
+        {
+            motherBoard = new MotherBoard();
+        }
+    }
+}
